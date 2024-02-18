593e914c61ec15fa5874d0016dbf4d89fdc9e28f
Herbert Parasio <91493748+Herbertph@users.noreply.github.com>
2024-02-18 02:30:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-openai_client = OpenAI(api_key="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")
+openai_client = OpenAI(api_key="-")
 
 @app.route('/gerar_historia', methods=['POST'])
 def gerar_historia():
